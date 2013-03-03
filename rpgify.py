50e88065ed7087881000b13228836026d4b6f111
edolfo <edolfo@gmail.com>
2013-03-03 00:50:05 -0500

    Jinja2 templates no longer use mustache templating to avoid collisions between jinja2 defaults and mustache defaults (in javascript templating)

--- rpgify.py
+++ rpgify.py
@@ -2,7 +2,18 @@ from flask import Flask
 import os, sys
 from controllers import root
 
-app = Flask('rpgify')
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+
+app = CustomFlask('rpgify')
 app.register_blueprint(root.views)
 
 if __name__ == "__main__":
